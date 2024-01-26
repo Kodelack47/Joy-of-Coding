e1701c8c278d5b2f829e0289cf04eeea815e272d
Kodelack47 <ousmanatunkara1@gmail.com>
2024-01-26 04:51:09 -0500

    py is fun

---
+++ main.py
@@ -0,0 +1,41 @@
+print("Hello World!")
+
+#print ("hello world")
+
+#x = 4 
+#y = x + 2
+
+#print(y)
+
+#import math  
+#print ( math.sqrt(25))
+
+#from math import sqrt
+
+#print(sqrt(25))
+
+import turtle 
+
+turtle.color("red")
+
+size = 100
+turtle.forward(size)
+turtle.right(120)
+turtle.forward(size)
+turtle.right(120)
+turtle.forward(size)
+turtle.right(120)
+
+#for i in range(1,11):
+ # print(i)
+  
+ # for <loop variable> in <sequence>:
+  # for char in "hello":
+  # print(char)
+#for i in range( 2, -48, -2) :
+  #print(i)
+#for letter in range(10) :
+  #print("hello world")
+  
+  for i in range( 2, -10, 4) :
+  print(i)
